94e99c9a357c57bd0485500d473938121804b701
Gillian Lu <lu.gillian.nicole@gmail.com>
2024-02-07 18:12:18 +0800

    2nd Commit

--- db/migrate/20240206112318_create_articles.rb
+++ db/migrate/20240206112318_create_articles.rb
@@ -1,8 +1,16 @@
 class CreateArticles < ActiveRecord::Migration[7.1]
+  #db:migrate to generate a table
   def change
     create_table :articles do |t|
+      t.string :title
+      t.text :body
 
       t.timestamps
     end
   end
+
+  # def down
+  #   drop_table :articles
+  # end
+
 end
---
+++ db/migrate/20240207083008_add_author_name_to_articles.rb
@@ -0,0 +1,9 @@
+# bin/rails g migration add_author_name_to_articles author_name:string
+class AddAuthorNameToArticles < ActiveRecord::Migration[7.1]
+  def change
+    #to change it to something else for ex, writer_name
+    #bin/rails db:rollback then make the change then migrate
+    #bin/rails g migration rename_author_name_to_writer_name
+    add_column :articles, :author_name, :string
+  end
+end
